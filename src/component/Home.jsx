1490b3bc5f4e6d5d300c6f8472b0f016c0253cb4
Frost <svet.tyshchyk@gmail.com>
2023-01-09 14:01:28 +0200

    fix: fix animation bug for span component & add delay

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,21 +3,32 @@ import logo from "../assets/images/logo-s.png"
 import {Link} from "react-router-dom";
 import AnimateLetter from "./UI/AnimatedLetters/AnimateLetter";
 import Logo from "./Logo";
+import {useEffect, useState} from "react";
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+          setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+
+    }, [])
+
+
     const nameArray = "viatoslav".split(""), jobTitleArray = "web developer".split("")
     return (
         <div className="container">
             <div className="home-page">
                 <div className="text-zone ">
                     <h1>
-                        <AnimateLetter letterClass="animate-letters_" stringArray={"Hi".split("")} startIndex={10}/>
+                        <AnimateLetter letterClass={letterClass} stringArray={"Hi".split("")} startIndex={10}/>
                         <br/>
-                        <AnimateLetter letterClass="animate-letters_" stringArray={"I'm".split("")} startIndex={10}/>
+                        <AnimateLetter letterClass={letterClass} stringArray={"I'm".split("")} startIndex={10}/>
                         <img src={logo} alt="logo"/>
-                        <AnimateLetter letterClass="animate-letters_" stringArray={nameArray}startIndex={14}/>
+                        <AnimateLetter letterClass={letterClass} stringArray={nameArray}startIndex={14}/>
                         <br/>
-                        <AnimateLetter letterClass="animate-letters_" stringArray={jobTitleArray} startIndex={10}/>
+                        <AnimateLetter letterClass={letterClass} stringArray={jobTitleArray} startIndex={10}/>
 
                     </h1>
                     <h2>Frontend Developer</h2>
