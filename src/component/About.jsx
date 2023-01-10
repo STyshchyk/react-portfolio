51eea35c08aba467515682de53ceb5a6b6ac44e9
Frost <svet.tyshchyk@gmail.com>
2023-01-10 17:56:51 +0200

    feat: update about me section with cv

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,7 +1,10 @@
 import AnimateLetter from "./UI/AnimatedLetters/AnimateLetter";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact} from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
+
+import cv from "../assets/S.Tyshchyk's CV.pdf"
 
 const About = () => {
 
@@ -14,62 +17,74 @@ const About = () => {
 
     }, [])
     return (
-        <div className="container">
-            <div className="about-page">
-                <div className="text-zone">
-                    <h1>
-                        <AnimateLetter
-                            letterClass={letterClass}
-                            stringArray={"About me".split("")}
-                            startIndex={10}
-                        />
-                    </h1>
-                    <p>
-                        I’m very ambitious front-end developer looking for a role in
-                        established IT company with the opportunity to work with the latest
-                        technologies on challenging and diverse projects.
-                    </p>
-                    <p>
-                        I have been working as a developer for MetaTrader for the last 3 years on a freelance basis,
-                        Upwork mainly. I developed various trading robots/scripts that simplify trading. Developing was
-                        performed using MQL language, it's very similar to C++.. Also, I have experience in QA, I worked
-                        for Gameloft company in 2019 in Lviv's office.
-                    </p>
+        <>
 
-                </div>
-                <div className="cube-zone">
-                    <section className="galaxy-header">
-                        <div className="galaxy">
-                            <div className="stars"></div>
-                        </div>
-                    </section>
-                    <div className="center-cube">
-                        <div className="cubespinner">
-                            <div className="face1">
-                                <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
-                            </div>
-                            <div className="face2">
-                                <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
-                            </div>
-                            <div className="face3">
-                                <FontAwesomeIcon icon={faCss3} color="#28A4d9"/>
-                            </div>
-                            <div className="face4">
-                                <FontAwesomeIcon icon={faReact} color="#5ed3f4"/>
-                            </div>
-                            <div className="face5">
-                                <FontAwesomeIcon icon={faJsSquare} color="#efd81d"/>
-                            </div>
-                            <div className="face6">
-                                <FontAwesomeIcon icon={faGitAlt} color="#ec4488"/>
-                            </div>
-
-                        </div>
+            <div className="container">
+                <div className="about-page">
+                    <div className="text-zone">
+                        <h1>
+                            <AnimateLetter
+                                letterClass={letterClass}
+                                stringArray={"About me".split("")}
+                                startIndex={10}
+                            />
+                        </h1>
+                        <p>
+                            I’m very ambitious front-end developer looking for a role in
+                            established IT company with the opportunity to work with the latest
+                            technologies on challenging and diverse projects.
+                        </p>
+                        <p>
+                            I have been working as a developer for MetaTrader for the last 3 years on a freelance basis,
+                            Upwork mainly. I developed various trading robots/scripts that simplify trading. Developing
+                            was
+                            performed using MQL language, it's very similar to C++.. Also, I have experience in QA, I
+                            worked
+                            for Gameloft company in 2019 in Lviv's office.
+                        </p>
+                        <p>
+                            To get more info about me, check out the CV
+                        </p>
+                        <a href={cv} target="_blank" className="flat-button">
+                            Download cv
+                        </a>
                     </div>
+                    <div className="cube-zone">
+                        <section className="galaxy-header">
+                            <div className="galaxy">
+                                <div className="stars"></div>
+                            </div>
+                        </section>
+                        <div className="center-cube">
+                            <div className="cubespinner">
+                                <div className="face1">
+                                    <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
+                                </div>
+                                <div className="face2">
+                                    <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+                                </div>
+                                <div className="face3">
+                                    <FontAwesomeIcon icon={faCss3} color="#28A4d9"/>
+                                </div>
+                                <div className="face4">
+                                    <FontAwesomeIcon icon={faReact} color="#5ed3f4"/>
+                                </div>
+                                <div className="face5">
+                                    <FontAwesomeIcon icon={faJsSquare} color="#efd81d"/>
+                                </div>
+                                <div className="face6">
+                                    <FontAwesomeIcon icon={faGitAlt} color="#ec4488"/>
+                                </div>
 
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
+            <Loader type="ball-pulse-sync" active/>
+
+        </>
     );
 };
 
---
+++ src/component/Contact.jsx
@@ -0,0 +1,93 @@
+import React, {useEffect, useRef, useState} from 'react';
+import Loader from "react-loaders";
+import AnimateLetter from "./UI/AnimatedLetters/AnimateLetter";
+import emailjs from '@emailjs/browser'
+
+
+
+import {Marker, Popup, MapContainer, TileLayer } from "react-leaflet";
+
+const Contact = () => {
+
+    const position = [49.842957, 24.031111]
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef();
+    useEffect(() => {
+        const myTimeOut = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 2000)
+
+        return clearTimeout(myTimeOut);
+    }, [])
+
+    function sendMail(e) {
+        e.preventDefault();
+        emailjs.sendForm(
+            "service_ov6gly7",
+            "template_x542bs9",
+            form.current,
+            "Fdl8bi92-kdRyuUW2"
+        ).then((result) => {
+            alert("Message sent")
+        }, (error) => {
+            alert("Error sending mail")
+        });
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="contact-page">
+                    <div className="text-zone">
+                        <h1>
+                            <AnimateLetter
+                                letterClass={letterClass}
+                                stringArray={"Contact me".split("")}
+                                startIndex={10}
+                            />
+                        </h1>
+                        <div className="contact-form">
+                            <form ref={form} onSubmit={sendMail}>
+                                <div className="row">
+                                    <input type="email" name="email" placeholder="Email" required/>
+                                    <input type="text" name="name" placeholder="Name" required/>
+                                </div>
+                                <input type="text" name="subject" placeholder="Subject" required/>
+                                <textarea placeholder={"Message"} name={"Text area"} required></textarea>
+                                <input type="submit" className="flat-button" value={"Send"}/>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="info-map">
+                        <div className="contact-info">
+                            Sviatoslav Tyshchyk,
+                            <br/>
+                            Ukraine, Lviv
+                            <br/>
+                            <br/>
+                            <span>Front-end developer</span>
+                        </div>
+                        <div className="map-wrapper">
+                            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                                <TileLayer
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={position}>
+                                    <Popup>
+                                        Ukraine. <br /> Lviv.
+                                    </Popup>
+                                </Marker>
+                            </MapContainer>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <Loader type="ball-pulse-sync" active/>
+
+        </>
+    );
+};
+
+export default Contact;
