e2d13c5b808909649878294d2c9cadb500929c85
Frost <svet.tyshchyk@gmail.com>
2023-01-17 16:08:35 +0200

    feat: add portfolio section

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -4,21 +4,20 @@ import portfolio from "../portfolio";
 
 const Portfolio = () => {
     const port = portfolio;
-    console.log(port)
     return (
         <div className={"container"}>
             <div className="portfolio-page">
                 <div className="card-wrapper">
                     {port.map((elem) => {
                         return <>
-                            <div className="flip-card">
+                            <div className="flip-card" key={elem.name}>
                                 <div className="flip-card-inner">
                                     <div className="flip-card-front">
                                         <img src={Object.values(elem.thumbnail)[0]} alt="all logo"/>
                                     </div>
                                     <div className="flip-card-back">
                                         <p>{elem.text}</p>
-                                        <a href={elem.link} target={"_blank"} className="btn-link">Check out</a>
+                                        <a href={elem.link} target={"_blank"} className="btn-link" rel={"noreferrer"}>Check out</a>
                                     </div>
                                 </div>
                             </div>
